4b3b9c4a6b5ee9df57b91dcfd744a2d4c3b6e145
preetimda <141523597+preetimda@users.noreply.github.com>
2026-04-29 22:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,24 +98,37 @@ payBtn.addEventListener("click", function () {
         paymentStatus: "Paid"
       };
 
-      try {
-  const res = await fetch("https://script.google.com/macros/s/AKfycbzoq9k_D4K5FVX6vHeG3uhj1EnK_3NyeLhwBEEUnK5AxNFMz_qOhGdi121fCC2sjqHM/exec", {
-    method: "POST",
-    body: JSON.stringify(orderData)
-  });
+     try {
+        await fetch("https://script.google.com/macros/s/AKfycbzoq9k_D4K5FVX6vHeG3uhj1EnK_3NyeLhwBEEUnK5AxNFMz_qOhGdi121fCC2sjqHM/exec", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(orderData)
+        });
 
-  const text = await res.text();
-  console.log(text);
+        successMsg.innerHTML = `
+          ✨ Payment Successful! <br>
+          Your signed copy is being prepared with love. ♡
+        `;
 
-  successMsg.innerHTML = `
-    ✨ Payment Successful! <br>
-    Your signed copy is being prepared with love. ♡
-  `;
+        document.getElementById("custName").value = "";
+        document.getElementById("custEmail").value = "";
+        document.getElementById("custPhone").value = "";
+        document.getElementById("custAddress").value = "";
+        document.getElementById("pincode").value = "";
+        document.getElementById("qty").value = 1;
+        document.getElementById("termsCheck").checked = false;
+        document.getElementById("refundCheck").checked = false;
 
-} catch (error) {
-  alert("Payment successful, but order saving failed.");
-  console.error(error);
-}
+        updateTotal();
+
+      } catch (error) {
+        alert("Payment successful, but order saving failed.");
+        console.error(error);
+      }
+    },
+
 
     modal: {
       ondismiss: function () {
